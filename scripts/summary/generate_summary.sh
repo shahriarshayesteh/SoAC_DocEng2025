9cbc9e25589771f3e066ced137944c6ffe3bba01
Shahriar Shayesteh <shahriar.shayesteh92@gmail.com>
2025-06-04 18:32:12 -0400

    Fix paths in README and scripts

--- scripts/summary/generate_summary.sh
+++ scripts/summary/generate_summary.sh
@@ -10,7 +10,7 @@ PYTHON="python3"
 echo "[SUMMARY] Running LexRank with ${SUMMARY_LENGTH} sentences and ${NUM_WORKERS} workers..."
 mkdir -p "$OUTPUT_DIR"
 
-$PYTHON /data/sxs7285/Porjects_code/thesis/DocEng/SoAC-DocEng/src/summary/lexrank.py \
+$PYTHON src/summary/lexrank.py \
     --output_dir "$OUTPUT_DIR" \
     --splits train validation test \
     --sentences_count "$SUMMARY_LENGTH" \
